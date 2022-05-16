5ef209f44065a7626bf522fea4817ccd17072494
ljcmps01 <ljcmps01@gmail.com>
2022-05-16 19:47:39 -0300

    Funcion ordenar estructuras por marca

--- Parcial1/main.c
+++ Parcial1/main.c
@@ -2,9 +2,20 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define TAM 3
+
 float aplicarDescuento(float precioOriginal);
 int contarCaracteres(char *palabra,char caracter);
 
+typedef struct
+{
+    int id;
+    char procesador[20];
+    char marca[20];
+    float precio;
+}Notebook;
+
+int ordenarMarcas(Notebook *array,int size);
 
 int main()
 {
@@ -15,6 +26,20 @@ int main()
     char palabra[20]="hola mundo";
     char caracter='o';
     printf("Match caracteres: %d\n",contarCaracteres(palabra,caracter));
+
+    Notebook notebookArray[TAM]=
+    {
+        {100,"AMD1","lenovo",12000},
+        {101,"INTEL I3","asus",15000},
+        {102,"INTEL I5","lenovo",20000}
+    };
+
+    ordenarMarcas(notebookArray,TAM);
+
+    for(int i=0;i<TAM;i++)
+    {
+        printf("%s\n",notebookArray[i].marca);
+    }
 
 
     return 0;
@@ -39,4 +64,39 @@ int contarCaracteres(char *palabra,char caracter)
     }
 
     return cantidadCaracteres;
+}
+
+int ordenarMarcas(Notebook *array, int size)
+{
+    if(size<0)
+    {
+        printf("Tamanio invalido\n");
+        return 1;
+    }
+    Notebook aux;
+    for(int i=0;i<TAM-1;i++)
+    {
+        for(int j=i+1;j<TAM;j++)
+        {
+            int comparacion=strcmp(array[i].marca,array[j].marca);
+            if(comparacion>0)
+            {
+                aux=array[i];
+                array[i]=array[j];
+                array[j]=aux;
+            }
+            else
+            {
+                if(comparacion==0){
+                    if(array[i].precio>array[j].precio)
+                    {
+                        aux=array[i];
+                        array[i]=array[j];
+                        array[j]=aux;
+                    }
+                }
+            }
+        }
+    }
+    return 0;
 }
