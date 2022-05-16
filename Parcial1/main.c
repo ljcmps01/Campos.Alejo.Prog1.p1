135f2f1b39e3feb69bdd612acd07fd7c29132711
ljcmps01 <ljcmps01@gmail.com>
2022-05-16 19:31:51 -0300

    Funcion contarCaracteres

--- Parcial1/main.c
+++ Parcial1/main.c
@@ -1,15 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 float aplicarDescuento(float precioOriginal);
+int contarCaracteres(char *palabra,char caracter);
+
 
 int main()
 {
+    float precioBruto=200;
+    float precioDescuento=aplicarDescuento(precioBruto);
+    printf("Descuento: %.2f\n",precioDescuento);
 
-    printf("Descuento: %.2f\n",aplicarDescuento(200));
+    char palabra[20]="hola mundo";
+    char caracter='o';
+    printf("Match caracteres: %d\n",contarCaracteres(palabra,caracter));
+
+
     return 0;
 }
 
-float aplicarDescuento(float precioOriginal){
+float aplicarDescuento(float precioOriginal)
+{
     return precioOriginal*(100-5)/100;
+}
+
+int contarCaracteres(char *palabra,char caracter)
+{
+    int largo=strlen(palabra);
+    int cantidadCaracteres=0;
+
+    for(int i=0; i<largo;i++)
+    {
+        if(palabra[i]==caracter)
+        {
+            cantidadCaracteres++;
+        }
+    }
+
+    return cantidadCaracteres;
 }
